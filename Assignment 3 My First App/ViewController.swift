12c9d7854996e56976effc5202e36cedc53a7a6a
Joshua Vand der Most <joshua.vandermost@cambriancollege.ca>
2022-11-21 19:03:17 -0500

    Initial Commit

---
+++ Assignment 3 My First App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment 3 My First App
+//
+//  Created by Cambrian on 2022-11-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

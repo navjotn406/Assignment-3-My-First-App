0ac3efae09fe466abcd8f0e9b41a960ea772dcfb
navjotn406 <113278709+navjotn406@users.noreply.github.com>
2022-11-28 00:14:03 -0500

    initial commit (layout and buttons)

--- Assignment 3 My First App/ViewController.swift
+++ Assignment 3 My First App/ViewController.swift
@@ -7,13 +7,129 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController
+{
+    
+    @IBOutlet weak var calculatorWorkings: UILabel!
+    @IBOutlet weak var calculatorResults: UILabel!
+    
+    var workings:String = ""
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        clearAll()
     }
+    func clearAll()
+    {
+        workings = ""
+        calculatorWorkings.text = ""
+        calculatorResults.text = ""
+    }
+    @IBAction func allClear(_ sender: Any) {
+        clearAll()
+    }
+    
+    @IBAction func backTap(_ sender: Any)
+    {
+        
+    }
+    
+    func addToWorkings(value: String)
+    {
+        workings = workings + value
+        calculatorWorkings.text = workings
+    }
+    
+    @IBAction func percentTap(_ sender: Any)
+    {
+        addToWorkings(value: "%")
+    }
+    
+    @IBAction func divideTap(_ sender: Any)
+    {
+        addToWorkings(value: "/")
+    }
+    
+    @IBAction func intoTap(_ sender: Any)
+    {
+        addToWorkings(value: "*")
+    }
+        
+    @IBAction func subTap(_ sender: Any)
+    {
+        addToWorkings(value: "-")
+    }
+    
+    @IBAction func addTap(_ sender: Any)
+    {
+        addToWorkings(value: "+")
+    }
+    
+    @IBAction func equalTap(_ sender: Any)
+    {
+        addToWorkings(value: "=")
+    }
+    @IBAction func decimalTap(_ sender: Any)
+    {
+        addToWorkings(value: ".")
+    }
+    
+    @IBAction func zeroTap(_ sender: Any)
+    {
+        addToWorkings(value: "0")
+    }
+    
+    @IBAction func oneTap(_ sender: Any)
+    {
+        addToWorkings(value: "1")
+    }
+    
+    @IBAction func twoTap(_ sender: Any)
+    {
+        addToWorkings(value: "2")
+    }
+    
+    @IBAction func threeTap(_ sender: Any)
+    {
+        addToWorkings(value: "3")
+    }
+    
+    @IBAction func fourTap(_ sender: Any)
+    {
+        addToWorkings(value: "4")
 
+    }
+    
+    @IBAction func fiveTap(_ sender: Any)
+    {
+        addToWorkings(value: "5")
 
+    }
+    
+    @IBAction func sixTap(_ sender: Any)
+    {
+        addToWorkings(value: "6")
+
+    }
+    
+    @IBAction func sevenTap(_ sender: Any)
+    {
+        addToWorkings(value: "7")
+
+    }
+    
+    @IBAction func eightTap(_ sender: Any)
+    {
+        addToWorkings(value: "8")
+
+    }
+    @IBAction func nineTap(_ sender: Any)
+    {
+        addToWorkings(value: "9")
+
+    }
+    
 }
 
